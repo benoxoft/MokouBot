86c26feee3fac1fbefd8b788e5314c14ffce67c6
Benoit Paquet <benoxoft@gmail.com>
2017-07-15 14:35:31 -0400

    More resilient to errors, add config.py, download images

--- mokou.py
+++ mokou.py
@@ -1,19 +1,16 @@
-import discord
 import asyncio
-from chatterbot import ChatBot
 from concurrent.futures import ProcessPoolExecutor
+import os
+import sys
 
-API_KEY = ''
+import discord
+
+from config import chatbot, PROCESS_POOL_EXECUTOR_COUNT, WAIT_TIME_BEFORE_TYPING, WAIT_TIME_RESPONSE_READY
+
 
 print("Starting process")
 client = discord.Client()
-ppool = ProcessPoolExecutor(3)  # Create a ProcessPool with 2 processes
-
-chatbot = ChatBot(
-    'Mokou',
-    trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
-    storage_adapter="chatterbot.storage.MongoDatabaseAdapter"
-)
+ppool = ProcessPoolExecutor(PROCESS_POOL_EXECUTOR_COUNT)
 
 
 @client.event
@@ -30,32 +27,47 @@ def generate_response(message):
 
 async def reply_to_message(message):
     print("Got", message.content)
-    msg = message.content.lower().replace('mokou', '').replace(client.user.mention, '')
+    msg = message.content.lower().replace(client.user.name.lower(), '').replace(client.user.mention, '')
     response_gen = asyncio.get_event_loop().run_in_executor(ppool, generate_response, msg)
-    await asyncio.sleep(2)
+    await asyncio.sleep(WAIT_TIME_BEFORE_TYPING)
     while not response_gen.done():
-        await asyncio.sleep(1)
+        await asyncio.sleep(WAIT_TIME_RESPONSE_READY)
         await client.send_typing(message.channel)
     response = response_gen.result()
     print("Sending", response)
-    if client.user.mention in message.content:
-        await client.send_message(message.channel, response)
-    else:
-        await client.send_message(message.channel, message.author.mention + ' ' + str(response))
+    filename = ''
+    if ' <img>' in str(response):
+        response, filename = str(response).split(' <img>')
+    await client.send_message(message.channel, message.author.mention + ' ' + str(response))
+    print('file:', filename)
+    if filename and os.path.exists(os.path.join('images', filename)):
+        with open(os.path.join('images', filename), 'rb') as f:
+            await client.send_file(message.channel, f)
 
 
 @client.event
 async def on_message(message):
     if client.user.mention == message.author.mention:
         return
-    if not ('mokou' in message.content.lower() or client.user.mention in message.content):
+    if not (client.user.name.lower() in message.content.lower() or client.user.mention in message.content):
         return
 
     await reply_to_message(message)
 
 if __name__ == '__main__':
+    if not os.path.exists('API_KEY.txt'):
+        print("Couldn't find API_KEY.txt.")
+        print("Create a file named API_KEY.txt and paste your API key in the file to connect the bot to Discord")
+        sys.exit(0)
+
+    with open('API_KEY.txt', 'r') as api_key_file:
+        API_KEY = api_key_file.read().strip()
+
     while True:
         try:
             client.run(API_KEY)
-        except:
+        except Exception as e:
+            print(e)
+            import time
+            time.sleep(10)
             client = discord.Client()
--- train.py
+++ train.py
@@ -1,19 +1,23 @@
 import requests
 import json
+import os
+import shutil
 import time
 from html.parser import HTMLParser
-from chatterbot import ChatBot
 
-chatbot = ChatBot(
-    'Mokou',
-    trainer='chatterbot.trainers.ListTrainer',
-    storage_adapter="chatterbot.storage.MongoDatabaseAdapter"
-)
-chatbot.storage.drop()
+from config import \
+    chatbot, \
+    SFW_BOARDS, \
+    NSFW_BOARDS, \
+    SLEEP_TIME_4CHAN, \
+    MIN_MESSAGE_LENGTH, \
+    MAX_MESSAGE_LENGTH, \
+    MIN_REPLY_LENGTH, \
+    MAX_REPLY_LENGTH
 
-BASE_THREADS_URL = "http://a.4cdn.org/{board}/threads.json"
-BASE_THREAD_CONTENT_URL = "http://a.4cdn.org/{board}/thread/{number}.json"
-BOARDS = ["a", "b", "g", "soc", "ck", "fit", "co", "v", "r9k", "s4s"]
+BASE_THREADS_URL = 'http://a.4cdn.org/{board}/threads.json'
+BASE_THREAD_CONTENT_URL = 'http://a.4cdn.org/{board}/thread/{number}.json'
+BASE_IMAGE_URL = 'http://i.4cdn.org/{board}/{filename}'
 
 
 # https://stackoverflow.com/questions/753052/strip-html-from-strings-in-python
@@ -30,12 +34,8 @@ class MLStripper(HTMLParser):
 
 def strip_tags(html):
     s = MLStripper()
-    s.feed(html.replace("<br>", " ").strip())
+    s.feed(html.replace('<br>', ' ').strip())
     return s.get_data()
-
-
-def get_board():
-    yield from BOARDS
 
 
 def get_board_threads(board):
@@ -55,9 +55,10 @@ def get_thread_content(board, thread):
         return
     for post in posts['posts']:
         if 'no' in post.keys() and 'com' in post.keys():
-            yield post['no'], post['com']
-        else:
-            print('unknown post:', post)
+            if 'tim' in post.keys() and 'ext' in post.keys():
+                yield post['no'], post['com'], str(post['tim']) + post['ext']
+            else:
+                yield post['no'], post['com'], ''
 
 
 def parse_post(content):
@@ -71,18 +72,32 @@ def parse_post(content):
             if len(tokens) > 1:
                 parsed_messages.append(tokens)
             else:
-                print("unknown tokens:", content)
+                print('unknown tokens:', content)
         return parsed_messages
+
+
+def save_image_file(board, filename):
+    if not filename:
+        return
+    if os.path.exists(os.path.join('images', filename)):
+        return
+    if 'webm' in filename:
+        return
+
+    r = requests.get(BASE_IMAGE_URL.format(board=board, filename=filename), stream=True)
+    with open(os.path.join('images', filename), 'wb') as out_file:
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, out_file)
 
 
 def get_messages_in_thread(board, thread):
     messages = dict()
-    for no, com in get_thread_content(board, thread):
+    for no, com, filename in get_thread_content(board, thread):
         parsed_com = parse_post(com)
         if parsed_com:
-            messages[str(no)] = {'com': strip_tags(parsed_com[0][1]), 'replies':[]}
+            messages[str(no)] = {'com': strip_tags(parsed_com[0][1]), 'replies':[], 'filename': filename}
         else:
-            messages[str(no)] = {'com': strip_tags(com), 'replies': []}
+            messages[str(no)] = {'com': strip_tags(com), 'replies': [], 'filename': filename}
 
         replies = parse_post(com)
         if replies:
@@ -91,36 +106,77 @@ def get_messages_in_thread(board, thread):
                     continue
                 reply_to, reply = data
                 if str(reply_to) in messages.keys():
-                    messages[str(reply_to)]['replies'].append(strip_tags(reply))
+                    messages[str(reply_to)]['replies'].append((strip_tags(reply), filename))
     return messages
 
 
-def learn(message, response):
-    if len(message) > 256 or len(response) > 256 or len(message) < 4 or len(response) < 4:
-        return
+def learn(message, msg_filename, response, filename):
     print('learning', message, response)
+    if msg_filename:
+        message += ' <img>' + msg_filename
+    if filename:
+        response += ' <img>' + filename
+
     chatbot.train([message, response])
 
 
-def main():
-    for board in BOARDS:
+def create_training_dataset(boards, save_images):
+    training_data = []
+
+    if not os.path.exists('images'):
+        os.mkdir('images')
+
+    for board in boards:
         time.sleep(1)
         threads = get_board_threads(board)
         time.sleep(1)
 
         for thread in threads:
             start_time = time.time()
-            print('Reading thread', thread, 'from', board)
+            print('Reading thread', thread, 'from /' + board + '/')
+
             messages = get_messages_in_thread(board, thread)
+
             for id in messages.keys():
                 message = messages[id]
-                if len(message['replies']) > 0:
-                    for reply in message['replies']:
-                        learn(message['com'], reply)
-            sleep_time = 1.5 - (time.time() - start_time)
+                if len(message['replies']) == 0:
+                    continue
+                for reply, filename in message['replies']:
+                    if (len(message['com']) > MAX_MESSAGE_LENGTH or
+                                len(reply) > MAX_REPLY_LENGTH or
+                                len(message['com']) < MIN_MESSAGE_LENGTH or
+                                len(reply) < MIN_REPLY_LENGTH):
+                        continue
+                    training_data.append([message['com'], message['filename'], reply, filename])
+
+                    if save_images:
+                        save_image_file(board, filename)
+
+            sleep_time = SLEEP_TIME_4CHAN - (time.time() - start_time)
             if sleep_time > 0:
                 print('Sleeping', sleep_time)
                 time.sleep(sleep_time)
+    return training_data
+
+
+def learn_from_dataset(training_data):
+    for message, msg_filename, response, filename in training_data:
+        learn(message, msg_filename, response, filename)
+
+
+def main():
+    sfw_data = create_training_dataset(SFW_BOARDS, True)
+    with open('sfw_data.json', 'w') as datafile:
+        json.dump(sfw_data, datafile)
+
+    nsfw_data = create_training_dataset(NSFW_BOARDS, False)
+    with open('nsfw_data.json', 'w') as datafile:
+        json.dump(nsfw_data, datafile)
+
+    chatbot.storage.drop()
+    learn_from_dataset(sfw_data)
+    learn_from_dataset(nsfw_data)
+
 
 if __name__ == '__main__':
     main()
